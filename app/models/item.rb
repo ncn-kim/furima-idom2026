60acab8e305ef2a8ed961af0604a58b7f2fe809d
ncn-kim <ncn001001@gmail.com>
2026-06-23 11:41:09 +0900

    itemsテーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :schedule
+
+  # active_hash外のカラムバリデーション
+  validates :name, :detail, :price, :status, presence: true
+  # active_hash発カラムバリデーション("---"保存不可設定)
+  validates :category_id, :sales_status_id, :shipping_fee_id, :prefecture_id, :schedule_id,
+            numericality: { other_than: 1, message: "can't be blank" }
+  enum status: {
+    sold_out: 0,
+    in_stock: 1
+  }
 end
--- db/migrate/20260623021020_create_items.rb
+++ db/migrate/20260623021020_create_items.rb
@@ -1,7 +1,17 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-
+      # ユーザー管理機能待ち
+      # t.references :user,       null:false, foreign_key: true
+      t.string :name, null: false
+      t.text :detail, null: false
+      t.integer :price, null: false
+      t.integer :status, null: false, default: 1
+      t.integer :category_id, null: false
+      t.integer :sales_status_id, null: false
+      t.integer :shipping_fee_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :schedule_id, null: false
       t.timestamps
     end
   end
