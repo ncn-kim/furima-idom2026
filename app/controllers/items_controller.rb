34b6c9320b36778429ea73c70a964442578ab6f0
ncn-kim <ncn001001@gmail.com>
2026-06-24 17:08:58 +0900

    ログインしなくても見れるように設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: %i[index]
   def index
     @items = Item.order('created_at DESC')
     # includes(:user) ユーザー管理機能マージ時反映
