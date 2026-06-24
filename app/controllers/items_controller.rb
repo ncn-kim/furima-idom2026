6653f741eb32b3322f4aea97fd825740d2c94bf0
m12031004 <misaki_tsuchimoto@mail.ncn-ncc.co.jp>
2026-06-24 15:06:42 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,21 @@
 class ItemsController < ApplicationController
+  #ログイン有無確認
+  before_action :authenticate_user!
+  #destroy実行前に自分の商品か確認
+  before_action :authorize_destroy!, only: [:destroy]
+
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+  def authorize_destroy!
+    @item = Item.find(params[:id])
+    #自分の商品でない場合トップページに
+    redirect_to root_path unless @item.owned_by?(current_user)
+  end
+
 end
