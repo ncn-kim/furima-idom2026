2f6d0be073f2b792935d01854c232ae8d2d6081a
ncn-kim <ncn001001@gmail.com>
2026-06-25 09:45:13 +0900

    index画面backgroundimage反映できるよう修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users  
+  devise_for :users
   root "items#index"
   resources :items do
         resources :orders, only: [:index, :create], path: "orders"
